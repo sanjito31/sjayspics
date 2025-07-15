7e9b626a212a726d88bc200247d0efac8a52d182
Sanjay Kumar <sanjayvkumar31@gmail.com>
2025-07-15 15:54:41 -0400

    dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
 import { IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google"
 import "./globals.css";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import { ThemeProvider } from "@/components/theme-provider";
 
 const ibmSans = IBM_Plex_Sans({
   variable: "--font-IBM-sans",
@@ -36,11 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body
-        className={`${ibmSans.variable} ${ibmMono.variable} antialiased`}
+        className={`${ibmSans.variable} ${ibmMono.variable} 
+                    antialiased
+                    bg-background`}
       >
-        {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+          {children}
+          </ThemeProvider>
       </body>
     </html>
   );
