cfa37697956e87030f8f152b912cd1b0674a6ac9
Sanjay Kumar <sanjayvkumar31@gmail.com>
2025-08-14 15:02:17 -0400

    homepage mobile optimization

--- components/GalleryCard.tsx
+++ components/GalleryCard.tsx
@@ -14,9 +14,9 @@ export default function GalleryCard( { photo, idx, onImageClick }: GalleryCardPr
 
 
     return (
-        <div className="grid grid-cols-5 gap-4 space-y-2">         
+        <div className="md:grid md:grid-cols-5 md:gap-4 space-y-2">         
             <div
-                className="cursor-pointer col-start-2 col-span-3"
+                className="cursor-pointer md:col-start-2 md:col-span-3 px-4 md:px-0"
                 onClick={() => onImageClick(photo)}>
                 <Image
                     className="w-full h-auto"
@@ -31,27 +31,30 @@ export default function GalleryCard( { photo, idx, onImageClick }: GalleryCardPr
                     />
             </div>
 
-    
-            <div className="col-start-5 col-span-1 ml-3 text-[14px] font-extrabold">
-                <h1 className="">{photo.title.toUpperCase()}</h1><br/>
-                <div className="text-gray-400">
-
-                <span><Camera size={16} className="inline-block align-middle mr-1 mb-0.5"/>
-                    {photo.exifData?.make} {photo.exifData?.model}</span><br/>
-                <span><Aperture size={16} className="inline-block align-middle mr-1 mb-0.5"/>
-                    {photo.exifData?.lens}</span><br/><br/>
-                <span>{photo.exifData?.shutterSpeed}s</span><br/>
-                <span>f/{photo.exifData?.aperture}</span><br/>
-                <span>ISO {photo.exifData?.iso}</span><br/><br/>
-                <span>{photo.exifData?.dateTaken?.toLocaleDateString()}</span><br/>
-                <br/>
-                <Link href={`/film/${photo.fujiData?.filmMode?.replaceAll(" ", "_")}`}
-                        className="hover:text-white">
-                    <span>{photo.fujiData?.filmMode}</span>
-                </Link>
-                <br/>
-                <br/>
-                <span className="font-light mt-4"><i>{photo.caption}</i></span><br/>
+            <div className="md:col-start-5 md:col-span-1 md:ml-3 text-[14px] font-extrabold px-4 md:px-0">
+                <div className="grid grid-cols-2 gap-4 md:block">
+                    <div>
+                        <h1 className="">{photo.title.toUpperCase()}</h1>
+                        <span className="font-light block"><i>{photo.caption}</i></span>
+                        <div className="text-gray-400">
+                            <span><Camera size={16} className="inline-block align-middle mr-1 mb-0.5"/>
+                                {photo.exifData?.make} {photo.exifData?.model}</span><br/>
+                            <span><Aperture size={16} className="inline-block align-middle mr-1 mb-0.5"/>
+                                {photo.exifData?.lens}</span>
+                        </div>
+                    </div>
+                    
+                    <div className="text-gray-400">
+                        <span>{photo.exifData?.shutterSpeed}s</span><br/>
+                        <span>f/{photo.exifData?.aperture}</span><br/>
+                        <span>ISO {photo.exifData?.iso}</span><br/><br/>
+                        <span>{photo.exifData?.dateTaken?.toLocaleDateString()}</span><br/>
+                        <br/>
+                        <Link href={`/film/${photo.fujiData?.filmMode?.replaceAll(" ", "_")}`}
+                                className="hover:text-white">
+                            <span>{photo.fujiData?.filmMode}</span>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
